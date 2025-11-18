4526ae9349cff70516d53a2ca89fc4db7d364075
avgduck <avgduck@gmail.com>
2025-11-17 20:54:06 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace HoldToPause;
+
+[BepInPlugin(GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string GUID = "avgduck.plugins.llb.holdtopause";
+    internal static Plugin Instance { get; private set; }
+    internal static ManualLogSource LogGlobal { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        LogGlobal = Logger;
+    }
+}
